967e886b53567eb11d210a4c1e58eb8d5a4e115a
kimaru355 <emmanuelkimaru355@gmail.com>
2025-05-29 00:43:54 +0300

    fix(contact form): add contact form api key

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const FORM_URL = 'https://api.web3forms.com/submit';
+const FORM_URL = 'https://formspree.io/f/xqaqrwbz';
 
 export const sendMessage = async (formData: FormData) => {
   const response = await axios.post(FORM_URL, formData);
